0376bac028092debfe2668f3e95ae9c0d32b15b8
Lmarkk <lassi.markkinen@hotmail.com>
2022-05-27 16:51:30 +0300

    feat: set up Vue project

---
+++ em-portfolio/src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from '@/components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <HelloWorld msg="Test" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+
+    </div>
+  </header>
+
+  <RouterView/>
+</template>
+
+<style>
+  @import '@/assets/styles.scss';
+</style>
---
+++ em-portfolio/src/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+</script>
+
+<template>
+  <h1 class="green">{{ msg }}</h1>
+</template>
+
+<style scoped>
+
+</style>
